b2a9deb1b1cfd863eeafe0a441fac8fd777f67a9
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-03-30 19:25:02 +0200

    add axios nel frontend

--- frontend/app.js
+++ frontend/app.js
@@ -1,2 +1,11 @@
-const h1 = document.getElementById('title')
+const axios = require('axios/dist/browser/axios.cjs');
+const ul = document.getElementById('title')
+const li = document.createElement('li')
+let array = []
 
+axios.get(`http://localhost:3000/data/`)
+  .then(res => {
+
+    array = res.data
+    console.log(array)
+  })
